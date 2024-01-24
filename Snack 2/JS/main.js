8c53026d92794f7d5d17e7959cf932f2cc8f8274
Christian-Lepidi <christianlepidi@gmail.com>
2024-01-24 16:02:45 +0100

    feat: proof of changes

---
+++ Snack 2/JS/main.js
@@ -0,0 +1,12 @@
+const allStudents = [
+  "Marco della Rovere",
+  "Paola Cortellessa",
+  "Andrea Mantegna",
+  "Gaia Borromini",
+  "Luigi Grimaldello",
+  "Piero della Francesca",
+  "Francesca da Polenta",
+];
+
+const nomiInMaiuscolo = allStudents.map((nomi) => nomi.toUpperCase());
+console.log(nomiInMaiuscolo);
