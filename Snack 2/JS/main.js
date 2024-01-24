2c914c519955e182860ee56b4c016c815755422c
Christian-Lepidi <christianlepidi@gmail.com>
2024-01-24 16:46:10 +0100

    feat: add. id and vote

--- Snack 2/JS/main.js
+++ Snack 2/JS/main.js
@@ -1,11 +1,25 @@
 const allStudents = [
+  " 213 ",
   "Marco della Rovere",
+  "78",
+  "110",
   "Paola Cortellessa",
+  "96",
+  "250",
   "Andrea Mantegna",
+  "48",
+  "145",
   "Gaia Borromini",
+  "74",
+  "196",
   "Luigi Grimaldello",
+  "68",
+  "102",
   "Piero della Francesca",
+  "50",
+  "120",
   "Francesca da Polenta",
+  "84",
 ];
 
 const nomiInMaiuscolo = allStudents.map((nomi) => nomi.toUpperCase());
@@ -14,7 +28,8 @@ console.log(nomiInMaiuscolo);
 const studenti = allStudents.map((studente) => {
   return {
     id: 213,
-    nome: "Marco della rovere",
+    nome: studente,
     voto: 78,
   };
 });
+console.log(stu);
