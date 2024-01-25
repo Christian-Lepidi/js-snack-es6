c0a8b23b28e3e38ed5c6c7845a82a40b469575d0
Christian-Lepidi <christianlepidi@gmail.com>
2024-01-25 12:27:01 +0100

    feat: add. third and fourth snacks

---
+++ Snack 3/JS/main.js
@@ -0,0 +1,25 @@
+const bicycles = [
+  {
+    nome: "bike",
+    peso: 80,
+  },
+  {
+    nome: "mountainbike",
+    peso: 72,
+  },
+  {
+    nome: "crossbike",
+    peso: 81,
+  },
+  {
+    nome: "spinningbike",
+    peso: 63,
+  },
+  {
+    nome: "hydrobike",
+    peso: 51,
+  },
+];
+
+const [firstbike, secondbike, thirdbike, fourthbike, fifthbike] = bicycles;
+console.log(firstbike, secondbike, thirdbike, fourthbike, fifthbike);
